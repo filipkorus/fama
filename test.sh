891d10fc42f59104bfa1d58e611f6eafef3322a6
Filip Korus <filipkorus123@gmail.com>
2025-11-15 19:18:25 +0100

    Replaced deprecated functions and other minor fixes :)

--- test.sh
+++ test.sh
@@ -10,3 +10,7 @@ docker-compose -f docker-compose.test.yml build --no-cache
 echo ""
 echo "[*] Running all tests..."
 docker-compose -f docker-compose.test.yml up
+
+echo ""
+echo "[*] Tests completed."
+docker-compose -f docker-compose.test.yml down
